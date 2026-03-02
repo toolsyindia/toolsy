bc6fd355b39e178533c87111feab3e8fa9ab63d5
toolsyindia <ferocioustelugu@gmail.com>
2026-03-02 07:43:34 +0530

    fix: explicit base path for netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ADD THIS LINE (Tells Vite to serve assets from the root)
   server: {
     host: "::",
     port: 8080,
